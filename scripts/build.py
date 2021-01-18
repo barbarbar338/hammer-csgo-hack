ba0310b2a96fb7664f57a27850952c91033ef44f
Barış DEMİRCİ <demirci.baris38@gmail.com>
2021-01-18 16:42:49 +0300

    🎨 Update folder structure

--- scripts/build.py
+++ scripts/build.py
@@ -6,5 +6,5 @@ if os.path.isdir("dist"):
     shutil.rmtree("dist")
 if os.path.isdir("build"):
     shutil.rmtree("build")
-subprocess.call(["pyinstaller", "--onefile", "--icon=hammer.ico", "hammer.py"])
-shutil.copy("config.json", "dist")
+subprocess.call(["pyinstaller", "--onefile", "--icon=assets/favicon.ico", "--name", "hammer", "main.py"])
+shutil.copy("config.yaml", "dist")
