207e22b9168405829ccadd83e22d6a2e3e3fc083
Barış DEMİRCİ <demirci.baris38@gmail.com>
2021-01-18 16:43:17 +0300

    🔧 Change to yaml files

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,4 @@
-import json
+import yaml
 from datetime import datetime
 
 version = "1.1.1"
@@ -6,8 +6,8 @@ latestKey = "squishy-rock"
 currentHash = "0baf43871a87eb55fcf33390a582c86f:2085d5ed0ebb04af1c79b22d"
 endDate = datetime(2050, 1, 15, 15, 30).timestamp()
 
-with open("config.json") as file:
-    config = json.load(file)
+with open("config.yaml") as file:
+    config = yaml.safe_load(file)
     aimlock = config["aimlock"]
     bhop = config["bhop"]
     glow = config["glow"]
