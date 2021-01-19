e210e50777a168832a1602d2cd5fc9626e6445e5
Barış DEMİRCİ <demirci.baris38@gmail.com>
2021-01-19 12:47:55 +0300

    🔨 Add prebuild and format scripts

---
+++ scripts/format.py
@@ -0,0 +1,10 @@
+import subprocess
+
+
+def format():
+    print("🎀 Formatting files")
+    subprocess.call(["python", "-m" "black", "."])
+
+
+if __name__ == "__main__":
+    format()
---
+++ scripts/prebuild.py
@@ -0,0 +1,15 @@
+import shutil
+import os
+
+
+def prebuild():
+    print('🧹 Cleaning "dist" directory')
+    if os.path.isdir("dist"):
+        shutil.rmtree("dist")
+    print('🧹 Cleaning "build" directory')
+    if os.path.isdir("build"):
+        shutil.rmtree("build")
+
+
+if __name__ == "__main__":
+    prebuild()
