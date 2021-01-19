2afe05a0f440861e7ea0e5a98848784a265b3fb3
Barış DEMİRCİ <demirci.baris38@gmail.com>
2021-01-19 12:06:58 +0300

    🎨 Format

--- scripts/build.py
+++ scripts/build.py
@@ -6,5 +6,14 @@ if os.path.isdir("dist"):
     shutil.rmtree("dist")
 if os.path.isdir("build"):
     shutil.rmtree("build")
-subprocess.call(["pyinstaller", "--onefile", "--icon=assets/favicon.ico", "--name", "hammer", "main.py"])
+subprocess.call(
+    [
+        "pyinstaller",
+        "--onefile",
+        "--icon=assets/favicon.ico",
+        "--name",
+        "hammer",
+        "main.py",
+    ]
+)
 shutil.copy("config.yaml", "dist")
