3ac609960f7f263e7e19bbe0a5333458c1fb9c6c
Barış DEMİRCİ <demirci.baris38@gmail.com>
2021-01-22 11:21:18 +0300

    🔧 Update version, key and hash

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,9 @@
 import yaml
 from datetime import datetime
 
-version = "2.0.0"
-latestKey = "spicy-donut"
-currentHash = "f162cf5126d44de878974b96f0cb97cb:63363e17ebcc6ca7d6384f"
+version = "2.1.0"
+latestKey = "disguised-shake"
+currentHash = "f5a402f614e08bfa25d786cc7bdf2d48:7351bb0aef6e05bc31085de2a43e84"
 endDate = datetime(2050, 1, 15, 15, 30).timestamp()
 
 with open("config.yaml") as file:
