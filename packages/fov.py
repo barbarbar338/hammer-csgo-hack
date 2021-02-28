99de33cbbbba68e613b5976b88ba7d56e76752ec
bckd00r <48347237+bckd00r@users.noreply.github.com>
2021-02-28 17:17:09 +0300

    Delete packages directory

--- packages/fov.py
+++
@@ -1,9 +0,0 @@
-from utils.dll import getPlayer, csgo
-from utils.offsets import netvars
-from utils.config import config
-
-
-def fov():
-    player = getPlayer()
-    if player:
-        csgo.write_int(player + netvars["m_iDefaultFOV"], config["default_fov"])
--- packages/radar.py
+++
@@ -1,9 +0,0 @@
-from utils.dll import csgo, client_dll
-from utils.offsets import signatures, netvars
-
-
-def radar():
-    for i in range(1, 32):
-        entity = csgo.read_int(client_dll + signatures["dwEntityList"] + i * 0x10)
-        if entity:
-            csgo.write_int(entity + netvars["m_bSpotted"], 1)
